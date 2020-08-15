6097837c97b03f84d5f9264e9a4634770884f075
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-08-15 16:43:03 -0300

    :truck: Added routes file.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-app.use(express.json());
 
-app.get('/users', (req, res) => {
-	return res.json({user: 'users'});
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
